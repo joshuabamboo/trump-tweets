f5a65f7bde55531fc084f6899df325bfbe33bba1
jprosario <joshuaowens011@gmail.com>
2017-11-22 15:54:31 -0500

    add view for when he hasnt tweeted

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,8 @@ class TweetsController < ApplicationController
     if Tweet.todays_tweets.present?
       @score = ((Tweet.daily_negatives.length/Tweet.todays_tweets.length.to_f).round(2)*100).to_i
       @worst = Tweet.daily_worst.content
+    else
+      render 'no-tweets.html.erb'
     end
   end
 end
