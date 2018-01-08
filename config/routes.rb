296d7ecddada73fa4b5523884f0025fdf96aaf07
jprosario <joshuaowens011@gmail.com>
2018-01-08 17:25:03 -0500

    holy shit a working version of d3 bar chart w sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tweets, only: :index
-  get '/data', to: 'tweets#data', as: 'data', :defaults => { :format => 'csv' }
+  get '/data', to: 'tweets#data', as: 'data', :defaults => { :format => 'json' }
 
   root 'tweets#index'
 end
