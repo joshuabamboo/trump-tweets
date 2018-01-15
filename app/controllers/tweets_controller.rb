cb7912c49ac1ac55e93a7b25cd5505e4e7b0d7db
jprosario <joshuaowens011@gmail.com>
2018-01-14 23:27:54 -0500

    add d3 dot plot

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,7 +13,6 @@ class TweetsController < ApplicationController
   def data
     tweets = Tweet.all
     @results = []
-
     # get all uniq dates
     dates = Tweet.order(date: :asc).pluck(:date).map {|date| date.strftime('%b %d, %Y')}.uniq
     # iterate over them
@@ -27,6 +26,19 @@ class TweetsController < ApplicationController
       # format.json {render json: @tweet}
       format.json {render json: @results}
       format.csv {render 'test.csv'}
+    end
+  end
+
+  def circle
+    tweets = Tweet.all
+    @results = [{year: '0'}]
+    weeks = Array( Date.parse("2017-01-20")..Date.parse("2018-01-14") ).select(&:sunday?).map(&:to_s)
+    weeks.each.with_index(1) do |d, i|
+      tweet_count = Tweet.where(:date => Date.parse(d).beginning_of_week..Date.parse(d).end_of_day).size
+      @results[0]["Week #{i}"] = tweet_count
+    end
+    respond_to do |format|
+      format.json {render json: @results}
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tweets, only: :index
   get '/data', to: 'tweets#data', as: 'data', :defaults => { :format => 'json' }
+  get '/circle', to: 'tweets#circle', as: 'circle', :defaults => { :format => 'json' }
 
   root 'tweets#index'
 end
