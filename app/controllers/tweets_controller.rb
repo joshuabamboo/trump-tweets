4e8612b11b9b5a79f56223b98b0c33ffda9387f5
Joshua Owens <joshuaowens011@gmail.com>
2018-02-09 16:58:36 -0500

    OMG! I DEFEATED D3! The dot plot is good. Feels like I just beat Bowser

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,8 +29,12 @@ class TweetsController < ApplicationController
     @results = [{year: '0'}]
     weeks = Array( Date.parse("2017-01-20")..Date.parse("2018-01-20") ).select(&:sunday?).map(&:to_s)
     weeks.each.with_index(1) do |d, i|
-      tweet_count = Tweet.where(:date => Date.parse(d).beginning_of_week..Date.parse(d).end_of_day).size
-      @results[0]["Week #{i}"] = tweet_count
+      tweets = Tweet.where(:date => Date.parse(d).beginning_of_week..Date.parse(d).end_of_day)#.sort_by {|t| t.negative?}
+      # do I want to sort them by negativity or time? or something else
+      sorted_tweets = tweets.where("negative is not null").order("negative desc")
+      no_prediction = tweets.where("negative is null")
+      formatted_tweets = sorted_tweets + no_prediction
+      @results[0]["Week #{i}"] = formatted_tweets
     end
     respond_to do |format|
       format.json {render json: @results}
